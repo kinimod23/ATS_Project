271cec9af80080daad1828628e704293c2f4c1ce
Samsella <untergasser@uni-potsdam.de>
2018-08-31 21:11:03 +0200

    minor

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -91,8 +91,10 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
                                       feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
                 else:
                     #graph = tf.get_default_graph()
-                    #encoder.x1 = graph.get_tensor_by_name
-                    merged, _, c, a = sess.run([encoder.merged, encoder.cost, encoder.acc],
+                    #encoder.x1 = graph.get_tensor_by_name('x1:0')
+                    #encoder.x2 = graph.get_tensor_by_name('x2:0')
+                    #encoder.y1 = graph.get_tensor_by_name('y1:0')
+                    merged, c, a = sess.run([encoder.merged, encoder.cost, encoder.acc],
                                       feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
                     #preds, acc_enc  = sess.run([encoder.prediction, encoder.acc],
                     #                  feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
