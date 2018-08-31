8785cde39f380fddee4930eba15b2549ce51ddc3
Samsella <untergasser@uni-potsdam.de>
2018-08-31 21:01:58 +0200

    minor

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -88,12 +88,14 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
                 x1, x2, y = train_data.next_batch(batch_size=batch_size)
                 if model_type == 'convolution':
                     merged, _, c, a = sess.run([encoder.merged, optimizer, encoder.cost, encoder.acc],
-                                    feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
+                                      feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
                 else:
-                    preds, acc_enc = sess.run([encoder.prediction, encoder.acc],
-                                    feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
-                    merged, _, c, a = sess.run([decoder.merged, optimizer, decoder.cost, decoder.acc],
-                                    feed_dict={decoder.x: preds, decoder.y: x2})
+                    merged, _, c, a = sess.run([encoder.merged, optimizer, encoder.cost, encoder.acc],
+                                      feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
+                    #preds, acc_enc  = sess.run([encoder.prediction, encoder.acc],
+                    #                  feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
+                    #merged, _, c, a = sess.run([decoder.merged, optimizer, decoder.cost, decoder.acc],
+                     #                 feed_dict={decoder.x: preds, decoder.y: x2})
                 MeanCost += c
                 MeanAcc += a
                 if i % 200 == 0:
