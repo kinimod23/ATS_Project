17730d499a4390a1936b67ef11c141af42c07115
Samsella <untergasser@uni-potsdam.de>
2018-09-02 15:02:04 +0200

    try to find variables to train

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -40,8 +40,9 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
 ############################################################################
 #########################      MODEL      ##################################
 ############################################################################
-
     tfconfig = tf.ConfigProto(allow_soft_placement = True)
+    sess = tf.Session(config=tfconfig)
+
     with tf.device("/gpu:0"):
 
         encoder = ABCNN_conv(s=train_data.max_len, w=w, l2_reg=l2_reg,
@@ -51,10 +52,9 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
         model_path = build_path("./models/", data, 'BCNN', num_layers, model_type, word2vec)
         model_path_old = build_path("./models/", data, 'BCNN', num_layers, 'convolution', word2vec)
 
-        with tf.Session(config=tfconfig) as sess:
-            if model_type == 'deconvolution':
-                saver.restore(sess, model_path_old + "-" + str(1000))
-                print(model_path + "-" + str(1000), "restored.")
+        if model_type == 'deconvolution':
+            saver.restore(sess, model_path_old + "-" + str(1000))
+            print(model_path + "-" + str(1000), "restored.")
 
         if model_type != 'convolution':
             decoder = ABCNN_deconv(s=train_data.max_len, w=w, l2_reg=l2_reg,
@@ -64,55 +64,54 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
         if model_type == 'convolution':
             optimizer = tf.train.AdagradOptimizer(lr, name="optimizer").minimize(encoder.cost)
         else:
-            optimizer = tf.train.AdagradOptimizer(lr, name="optimizer").minimize(decoder.cost)
+            optimizer = tf.train.AdagradOptimizer(lr, name="optimizer").minimize(decoder.cost, var_list=trainable_variables(scope='Decoder'))
 
-        init = tf.variables_initializer(trainable_variables)
+        init = tf.variables_initializer(tf.trainable_variables(scope='Decoder'))
 
 
 ############################################################################
 #########################     TRAINING     #################################
 ############################################################################
-    with tf.Session(config=tfconfig) as sess:
-        #if model_type == 'deconvolution':
-        #    saver.restore(sess, model_path_old + "-" + str(1))
-        #    print(model_path + "-" + str(1), "restored.")
-        train_summary_writer = tf.summary.FileWriter("../tf_logs/train2", sess.graph)
-        sess.run(init)
-        print("=" * 50)
-        for e in range(1, epoch + 1):
-            Sentences = []
-            print("[Epoch " + str(e) + "]")
-            train_data.reset_index()
-            i , MeanCost, MeanAcc = 0, 0, 0
-            while train_data.is_available():
-                i += 1
-                x1, x2, y = train_data.next_batch(batch_size=batch_size)
-                if model_type == 'convolution':
-                    merged, _, c, a = sess.run([encoder.merged, optimizer, encoder.cost, encoder.acc],
-                                      feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
-                else:
-                    #graph = tf.get_default_graph()
-                    #encoder.x1 = graph.get_tensor_by_name('x1:0')
-                    #encoder.x2 = graph.get_tensor_by_name('x2:0')
-                    #encoder.y1 = graph.get_tensor_by_name('y1:0')
-                    preds, acc_enc  = sess.run([encoder.prediction, encoder.acc],
-                                      feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
-                    merged, output, c, a = sess.run([decoder.merged, decoder.prediction, decoder.cost, decoder.acc],
-                                      feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y, decoder.x: preds, decoder.y: x2})
-                    Sentences.append(output)
-                MeanCost += c
-                MeanAcc += a
-                if i % 200 == 0:
-                    if model_type == 'deconvolution':
-                        print('encoder accuracy: {}'.format(acc_enc))
-                    print('[batch {}]  cost: {}  accuracy: {}'.format(i, c, a))
-                train_summary_writer.add_summary(merged, i)
-            print('Mean Cost: {}   Mean Accuracy: {}'.format(MeanCost/i, MeanAcc/i))
-            if e % 100 == 0:
-                save_path = saver.save(sess, build_path("./models/", data, 'BCNN', num_layers, model_type, word2vec), global_step=e)
-                print("model saved as", save_path)
-        print("training finished!")
-        print("=" * 50)
+    #if model_type == 'deconvolution':
+    #    saver.restore(sess, model_path_old + "-" + str(1))
+    #    print(model_path + "-" + str(1), "restored.")
+    train_summary_writer = tf.summary.FileWriter("../tf_logs/train2", sess.graph)
+    sess.run(init)
+    print("=" * 50)
+    for e in range(1, epoch + 1):
+        Sentences = []
+        print("[Epoch " + str(e) + "]")
+        train_data.reset_index()
+        i , MeanCost, MeanAcc = 0, 0, 0
+        while train_data.is_available():
+            i += 1
+            x1, x2, y = train_data.next_batch(batch_size=batch_size)
+            if model_type == 'convolution':
+                merged, _, c, a = sess.run([encoder.merged, optimizer, encoder.cost, encoder.acc],
+                                  feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
+            else:
+                #graph = tf.get_default_graph()
+                #encoder.x1 = graph.get_tensor_by_name('x1:0')
+                #encoder.x2 = graph.get_tensor_by_name('x2:0')
+                #encoder.y1 = graph.get_tensor_by_name('y1:0')
+                preds, acc_enc  = sess.run([encoder.prediction, encoder.acc],
+                                  feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y})
+                merged, output, c, a = sess.run([decoder.merged, decoder.prediction, decoder.cost, decoder.acc],
+                                  feed_dict={encoder.x1: x1, encoder.x2: x2, encoder.y1: y, decoder.x: preds, decoder.y: x2})
+                Sentences.append(output)
+            MeanCost += c
+            MeanAcc += a
+            if i % 200 == 0:
+                if model_type == 'deconvolution':
+                    print('encoder accuracy: {}'.format(acc_enc))
+                print('[batch {}]  cost: {}  accuracy: {}'.format(i, c, a))
+            train_summary_writer.add_summary(merged, i)
+        print('Mean Cost: {}   Mean Accuracy: {}'.format(MeanCost/i, MeanAcc/i))
+        if e % 100 == 0:
+            save_path = saver.save(sess, build_path("./models/", data, 'BCNN', num_layers, model_type, word2vec), global_step=e)
+            print("model saved as", save_path)
+    print("training finished!")
+    print("=" * 50)
     if model_type == 'deconvolution':
         fasttext = gensim.models.KeyedVectors.load("wiki.dump")
         print('FastText loaded')
