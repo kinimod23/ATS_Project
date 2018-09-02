c469e6ec0e75245b6b9d6af3331027f91126c1f4
Samsella <untergasser@uni-potsdam.de>
2018-09-02 19:03:15 +0200

    try to find the mistake

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -81,10 +81,10 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
                 print(v.name, v.shape)
             print("=" * 50)
             graph = tf.get_default_graph()
-            for v in sess.run(tf.report_uninitialized_variables()):
-                print(v)
-                print(str(v)[2:-1])
-                variables.append(graph.get_tensor_by_name(str(v)[2:-1]+':0'))
+            #for v in sess.run(tf.report_uninitialized_variables()):
+            #    print(v)
+            #    print(str(v)[2:-1])
+            #    variables.append(graph.get_tensor_by_name(str(v)[2:-1]+':0'))
 
         init = tf.variables_initializer(variables)
 
