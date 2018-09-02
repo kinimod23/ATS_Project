94bd025606983c7ed17267e44154fc8e28a1b920
Samsella <untergasser@uni-potsdam.de>
2018-09-02 11:32:26 +0200

    minor

--- ourABCNN/test_new.py
+++ ourABCNN/test_new.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 
 from preprocess_dump2 import Word2Vec, ComplexSimple, FastText
-from ABCNN_reduced import ABCNN
+from ABCNN_splitted import ABCNN_conv, ABCNN_deconv
 from utils import build_path
 import pickle
 import os
@@ -39,8 +39,8 @@ def test(w, l2_reg, epoch, max_len, model_type, data, word2vec, num_layers, num_
 ############################################################################
     tfconfig = tf.ConfigProto(allow_soft_placement = True)
     with tf.device("/gpu:0"):
-        model = ABCNN(s=test_data.max_len, w=w, l2_reg=l2_reg, model_type=model_type,
-                  num_features=test_data.num_features, num_classes=num_classes, num_layers=num_layers)
+        encoder = ABCNN_conv(s=train_data.max_len, w=w, l2_reg=l2_reg,
+                  num_layers=num_layers)
 
     model_path = build_path("./models/", data, 'BCNN', num_layers, model_type, word2vec)
     print("=" * 50)
@@ -60,13 +60,12 @@ def test(w, l2_reg, epoch, max_len, model_type, data, word2vec, num_layers, num_
             print("[Epoch " + str(e) + "]")
             test_data.reset_index()
             i, MeanCost = 0, 0
-            s1s, s2s, labels, features = test_data.next_batch(batch_size=test_data.data_size, model_type=model_type)
+            s1s, s2s, labels = test_data.next_batch(batch_size=test_data.data_size)
             for i in range(test_data.data_size):
-                pred, c, a = sess.run([model.prediction, model.cost, model.acc],
-                                           feed_dict={model.x1: np.expand_dims(s1s[i], axis=0),
-                                                      model.x2: np.expand_dims(s2s[i], axis=0),
-                                                      model.y: np.expand_dims(labels[i], axis=0),
-                                                      model.features: np.expand_dims(features[i], axis=0)})
+                pred, c, a = sess.run([encoder.prediction, encoder.cost, encoder.acc],
+                                           feed_dict={encoder.x1: np.expand_dims(s1s[i], axis=0),
+                                                      encoder.x2: np.expand_dims(s2s[i], axis=0),
+                                                      encoder.y: np.expand_dims(labels[i], axis=0)})
                 MeanCost += c
                 Accuracys.append(a)
                 Sentences.append(pred)
