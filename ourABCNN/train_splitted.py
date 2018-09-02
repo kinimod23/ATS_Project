be44748fecc604a65ed8017d9820cfc3eb1f7d91
Samsella <untergasser@uni-potsdam.de>
2018-09-02 18:00:37 +0200

    try to find the mistake

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -75,7 +75,7 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
             optimizer = opt.minimize(decoder.cost, var_list=tf.trainable_variables(scope='Decoder'))
             print("=" * 50)
             print("List of Variables:")
-            for v in tf.trainable_variables(scope='Decoder'):
+            for v in tf.trainable_variables(scope='Decoder').extend(list(opt._get_beta_accumulators())):
                 print(v.name, v.shape)
             print("=" * 50)
 
