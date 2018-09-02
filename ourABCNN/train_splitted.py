1334496abe204d57bfb17fb603aa00a73ae62e7a
Samsella <untergasser@uni-potsdam.de>
2018-09-02 16:57:34 +0200

    try to find the mistake

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -71,7 +71,7 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
                 print(v.name, v.shape)
             print("=" * 50)
         else:
-            opt = tf.train.AdagradOptimizer(lr, name="optimizer")
+            opt = tf.train.AdamOptimizer(lr, name="optimizer")
             optimizer = opt.minimize(decoder.cost, var_list=tf.trainable_variables(scope='Decoder'))
             print("=" * 50)
             print("List of Variables:")
@@ -79,7 +79,7 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
                 print(v.name, v.shape)
             print("=" * 50)
 
-        init = tf.variables_initializer(tf.trainable_variables(scope='Decoder'))
+        init = tf.variables_initializer([tf.trainable_variables(scope='Decoder'), tf.trainable_variables(scope='optimizer')])
 
 
 ############################################################################
