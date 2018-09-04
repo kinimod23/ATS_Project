52dbfb3cc98ff0776d93ab9de20819f4877a6eac
Samsella <untergasser@uni-potsdam.de>
2018-09-04 14:48:22 +0200

    updated deconvolution to get rid of image resize

--- ourABCNN/ABCNN_splitted.py
+++ ourABCNN/ABCNN_splitted.py
@@ -160,7 +160,7 @@ class ABCNN_deconv():
             with tf.name_scope("deconv"):
                 with tf.variable_scope("deconv") as scope:
                     deconv = tf.contrib.layers.conv2d_transpose(
-                    inputs= x,  num_outputs=1, kernel_size=(di,w), stride=int(d/di),
+                    inputs= x,  num_outputs=1, kernel_size=(di,w), stride=1, #int(d/di),
                     padding='SAME', data_format="NHWC", activation_fn=tf.nn.tanh,
                     weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                     weights_regularizer=tf.contrib.layers.l2_regularizer(scale=l2_reg),
@@ -171,7 +171,7 @@ class ABCNN_deconv():
                     tf.summary.histogram('weights', weights2)
                     biases2 = tf.get_variable('biases')
                     tf.summary.histogram('biases', biases2)
-                    deconv_trans = tf.transpose(deconv, [0, 3, 2, 1])
+                    #deconv_trans = tf.transpose(deconv, [0, 3, 2, 1])
                     return deconv_trans
 
         def DNN_layer(variable_scope, x, d):
