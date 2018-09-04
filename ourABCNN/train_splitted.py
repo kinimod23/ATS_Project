f028b9254e4fb484720f189a59cc7b6e41877638
Samsella <untergasser@uni-potsdam.de>
2018-09-04 18:06:14 +0200

    updated deconvolution to get rid of image resize

--- ourABCNN/train_splitted.py
+++ ourABCNN/train_splitted.py
@@ -64,8 +64,9 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
 
 
         if model_type == 'convolution':
-            optimizer = tf.train.AdamOptimizer(lr, name="optimizer").minimize(encoder.cost)
-            variables = tf.trainable_variables(scope='Encoder')
+            opt = tf.train.AdamOptimizer(lr, name="optimizer")
+            optimizer = opt.minimize(encoder.cost)
+            variables = tf.trainable_variables(scope='Encoder') + opt.variables()
             print("=" * 50)
             print("List of Variables:")
             for v in tf.trainable_variables():
