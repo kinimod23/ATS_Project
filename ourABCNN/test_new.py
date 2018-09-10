b84133f39b2956884d891d47e640c4b4086a09f2
Samsella <untergasser@uni-potsdam.de>
2018-09-10 21:15:28 +0200

    minor

--- ourABCNN/test_new.py
+++ ourABCNN/test_new.py
@@ -115,10 +115,9 @@ def test(w, l2_reg, epoch, max_len, model_type, data, word2vec, num_layers, num_
             for sen in Sentences[:2]:
                 string = ''
                 for word in range(50):
-                    print(sen.shape)
-                    print(sen[word,:].shape)
-                    print(type(sen[word,:]))
-                    string += fasttext.wv.similar_by_vector(sen[word,:], topn=1)[0][0] + ' '
+                    print(sen[0,word,:].shape)
+                    print(type(sen[0,word,:]))
+                    string += fasttext.wv.similar_by_vector(sen[0,:,word], topn=1)[0][0] + ' '
                 string += '\n'
                 f.write(string)
         print('Output created!')
