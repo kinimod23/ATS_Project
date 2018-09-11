61b25ec68dda25118f70fedae7b40aa4901129b7
Samsella <18039900+Samsella@users.noreply.github.com>
2018-09-11 13:04:57 +0200

    Delete testTheData.py

--- ourABCNN/testTheData.py
+++
@@ -1,6 +0,0 @@
-from preprocess import ComplexSimple, Word2Vec
-
-train_data = ComplexSimple(word2vec=Word2Vec())
-train_data.open_file(mode="train", method='labeled')
-
-batch_x1, batch_x2, batch_y, batch_features = train_data.next_batch(batch_size=1)
