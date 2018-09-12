c84c4ae5e12a0de478a3b622cd4f4dd122420098
Samsella <untergasser@uni-potsdam.de>
2018-09-12 21:21:07 +0200

    minor

--- ourABCNN/train.py
+++ ourABCNN/train.py
@@ -54,7 +54,7 @@ def train(lr, w, l2_reg, epoch, model_type, data, word2vec, batch_size, num_laye
             decoder = ABCNN_deconv(lr=lr, s=train_data.max_len, w=w, l2_reg=l2_reg,
                       num_layers=num_layers)
 
-        opt = tf.train.AdagradOptimizer(lr, name="optimizer")
+        opt = tf.train.AdamOptimizer(lr, name="optimizer")
         optimizer_enc = opt.minimize(encoder.cost)
         variables_enc = tf.trainable_variables(scope='Encoder')
         enc_saver = tf.train.Saver(var_list = variables_enc, max_to_keep=2)
