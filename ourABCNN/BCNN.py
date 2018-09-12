29d12c9feed926474354e1cf32299a38e6d1d00e
Samsella <untergasser@uni-potsdam.de>
2018-09-12 20:45:01 +0200

    minor

--- ourABCNN/BCNN.py
+++ ourABCNN/BCNN.py
@@ -166,22 +166,26 @@ class ABCNN_deconv():
                         DI = DNN_layer(variable_scope="DNN-"+str(i+2), x=DNNs[i], d=di)
                         DNNs.append(DI)
                 #DO = DNN_layer(variable_scope='DNN-'+str(num_layers), x=DNNs[-1], d=d0)
-                DO = tf.layers.dense(tf.reshape(DNNs[-1], [-1, 50*50]), 1024, activation=tf.nn.tanh,
+                FC = tf.layers.dense(tf.reshape(DNNs[-1], [-1, 50*50]), 1024, activation=tf.nn.tanh,
                     kernel_initializer=tf.random_uniform_initializer(minval=-0.2, maxval=0.2),
                     bias_initializer=tf.constant_initializer(0.01), name="FullConn")
+                DO = tf.layers.dense(FC, [d0, s], kernel_initializer=tf.random_uniform_initializer(minval=-0.1, maxval=0.1),
+                         bias_initializer=tf.zeros_initializer, name="logits")
                 DNNs.append(DO)
             else:
                 #DO = DNN_layer(variable_scope='DNN-'+str(num_layers-1), x=self.x, d=d0)
-                DO = tf.layers.dense(tf.reshape(DNNs[-1], [-1, 50*50]), 1024, activation=tf.nn.tanh,
+                FC = tf.layers.dense(tf.reshape(DNNs[-1], [-1, 50*50]), 1024, activation=tf.nn.tanh,
                     kernel_initializer=tf.random_uniform_initializer(minval=-0.2, maxval=0.2),
                     bias_initializer=tf.constant_initializer(0.01), name="FullConn")
+                DO = tf.layers.dense(FC, [d0, s], kernel_initializer=tf.random_uniform_initializer(minval=-0.1, maxval=0.1),
+                         bias_initializer=tf.zeros_initializer, name="logits")
                 DNNs.append(DO)
 
             with tf.variable_scope('Cost'):
-                self.acc = (cos_sim(tf.squeeze(DNNs[-1], axis=3), self.y))
+                self.acc = (cos_sim(DNNs[-1], self.y))
                 self.cost = 1-self.acc
                 tf.summary.scalar("cost", self.cost)
-            self.prediction = tf.squeeze(DNNs[-1], axis=3)
+            self.prediction = DNNs[-1]
 
         self.merged = tf.summary.merge_all()
         print("=" * 50)
